65278616c48cd4f8fd90f0efbabee956974a6a56
MarquiseJBlue <54650536+MarquiseJBlue@users.noreply.github.com>
2020-11-10 09:46:24 -0600

    Create main.cpp

---
+++ Assignments/H03/main.cpp
@@ -0,0 +1,19 @@
+void Hand::Sort(){
+
+//for loops for sorting through the cards
+for(int i=0;i<Size():i++)
+{
+  for(int j=1;j<Size()-1;j++)
+  {
+      if(Cards[i]->rank>Cards[j+i]->rank)
+      {
+          cout <<"swapping\n;
+          
+          //swapping the actual positions being compared
+          Card*temp=Cards[i];
+          Cards[i]=Cards[j+i];
+          Cards[j+i]=temp;
+          }
+     }
+   }
+}
